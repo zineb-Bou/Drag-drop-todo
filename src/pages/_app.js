9d1eabb80c7a1b8ee25d0b12c2ae599da8776751
unknown <boutaazineb08@gmail.com>
2022-03-18 16:07:47 +0100

    Fix Nextjs  title warning.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/global.scss';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Todo app</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
