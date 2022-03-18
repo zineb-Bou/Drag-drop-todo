cf1679aea184213be4ffd72bb1b09ddf419e3326
unknown <boutaazineb08@gmail.com>
2022-03-18 16:22:28 +0100

    Fix typo error.

--- src/components/MainSection/mainSection.js
+++ src/components/MainSection/mainSection.js
@@ -2,7 +2,7 @@
 import { jsx } from '@emotion/react';
 import { css } from '@emotion/react';
 import TodoList from '../Todo_list/todoList';
-import TodoForm from '../Todo_form/TodoForm';
+import TodoForm from '../Todo_form/todoForm';
 import { TodoContextProvider } from '../../utils/todoContext';
 import { todoContainer } from './mainSection.css';
 import TodoFooter from '../Footer/todoFooter';
